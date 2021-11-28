7e36f9a553aae9267bb7879a6d9bda03ebec5a17
illbot <posoroboti123@gmail.com>
2021-11-28 16:02:12 +0100

    rgb detect new

--- app/rgb_detect.py
+++ app/rgb_detect.py
@@ -13,28 +13,28 @@ def rgb_detect(img):
     # Creating a result copy
     img_result = img_local.copy()
     # variable for number of faces
-    faces = 0
+    personnel_number = 0
 
     # Defining color ranges RGB
     # Using small range around Palette Human skin tone color
-    # skin1 (197,140,133)
-    # skin2 (236,188,180)
-    # skin3 (209,188,164)
-    # skin4 (161,102,94)
-    # skin5 (80,51,53)
-    # skin6 (89,47,42)
-    skin1_low = np.array([195, 137, 130])
-    skin1_high = np.array([200, 143, 135])
-    skin2_low = np.array([233, 185, 177])
-    skin2_high = np.array([238, 190, 167])
-    skin3_low = np.array([205, 185, 160])
-    skin3_high = np.array([213, 190, 167])
-    skin4_low = np.array([158, 98, 90])
-    skin4_high = np.array([165, 105, 100])
-    skin5_low = np.array([75, 46, 50])
-    skin5_high = np.array([85, 55, 57])
-    skin6_low = np.array([85, 34, 37])
-    skin6_high = np.array([93, 52, 47])
+    # skin1 RGB(197,140,133) -> HSV(7,32.5,77.3) ~ (7, 32, 77)
+    skin1_low = np.array([2, 27, 72])
+    skin1_high = np.array([12, 37, 82])
+    # skin2 RGB(236,188,180) -> HSV(9,23.7,92.5) ~ (9,24,93)
+    skin2_low = np.array([4, 19, 88])
+    skin2_high = np.array([14, 29, 98])
+    # skin3 RGB(209,188,164) -> HSV(32,21.5,82) ~ (32,22,82)
+    skin3_low = np.array([27, 17, 77])
+    skin3_high = np.array([37, 27, 87])
+    # skin4 RGB(161,102,94) -> HSV(7,41.6,63.1) ~ (7,42,63)
+    skin4_low = np.array([2, 37, 58])
+    skin4_high = np.array([12, 47, 68])
+    # skin5 RGB(80,51,53) -> HSV(356,36.2,31.4) ~ (356,36,31)
+    skin5_low = np.array([351, 31, 26])
+    skin5_high = np.array([361, 41, 36])
+    # skin6 RGB(89,47,42) -> HSV(6,52.8,34.9) ~ (6,53,35)
+    skin6_low = np.array([1, 48, 30])
+    skin6_high = np.array([11, 58, 40])
 
     # Creating masks
     mask_skin1 = cv2.inRange(image_hsv, skin1_low, skin1_high)
@@ -44,62 +44,32 @@ def rgb_detect(img):
     mask_skin5 = cv2.inRange(image_hsv, skin5_low, skin5_high)
     mask_skin6 = cv2.inRange(image_hsv, skin6_low, skin6_high)
 
+    # Adding masks
+    mask = mask_skin1 + mask_skin2 + mask_skin3 + mask_skin4 + mask_skin5 + mask_skin6
+
     # Searching for human skin colors
     # Should return image with face, rest is black
-    img_skin1 = cv2.bitwise_and(image_hsv, image_hsv, mask=mask_skin1)
-    img_skin2 = cv2.bitwise_and(image_hsv, image_hsv, mask=mask_skin2)
-    img_skin3 = cv2.bitwise_and(image_hsv, image_hsv, mask=mask_skin3)
-    img_skin4 = cv2.bitwise_and(image_hsv, image_hsv, mask=mask_skin4)
-    img_skin5 = cv2.bitwise_and(image_hsv, image_hsv, mask=mask_skin5)
-    img_skin6 = cv2.bitwise_and(image_hsv, image_hsv, mask=mask_skin6)
+    img_skin = cv2.bitwise_and(image_hsv, image_hsv, mask=mask)
 
     # Creating grey images for contours
-    gray_skin1 = cv2.cvtColor(cv2.cvtColor(img_skin1, cv2.COLOR_HSV2BGR), cv2.COLOR_BGR2GRAY)
-    gray_skin2 = cv2.cvtColor(cv2.cvtColor(img_skin2, cv2.COLOR_HSV2BGR), cv2.COLOR_BGR2GRAY)
-    gray_skin3 = cv2.cvtColor(cv2.cvtColor(img_skin3, cv2.COLOR_HSV2BGR), cv2.COLOR_BGR2GRAY)
-    gray_skin4 = cv2.cvtColor(cv2.cvtColor(img_skin4, cv2.COLOR_HSV2BGR), cv2.COLOR_BGR2GRAY)
-    gray_skin5 = cv2.cvtColor(cv2.cvtColor(img_skin5, cv2.COLOR_HSV2BGR), cv2.COLOR_BGR2GRAY)
-    gray_skin6 = cv2.cvtColor(cv2.cvtColor(img_skin6, cv2.COLOR_HSV2BGR), cv2.COLOR_BGR2GRAY)
+    gray_skin = cv2.cvtColor(cv2.cvtColor(img_skin, cv2.COLOR_HSV2RGB), cv2.COLOR_RGB2GRAY)
+
+    # Canny edge detect on gray image
+    canny_img = cv2.Canny(gray_skin, 1, 1)
+
+    # Dilate / Erode to sum smaller areas
+    kernel_dil = np.ones((10, 10), np.uint8)
+    kernel_erode = np.ones((9, 9), np.uint8)
+    dilated_img = cv2.dilate(canny_img, kernel_dil, 1)
+    eroded_img = cv2.erode(dilated_img, kernel_erode, 1)
 
     # Searching for contours
-    contours_skin1, hierarchy = cv2.findContours(gray_skin1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    contours_skin2, hierarchy = cv2.findContours(gray_skin2, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    contours_skin3, hierarchy = cv2.findContours(gray_skin3, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    contours_skin4, hierarchy = cv2.findContours(gray_skin4, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    contours_skin5, hierarchy = cv2.findContours(gray_skin5, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    contours_skin6, hierarchy = cv2.findContours(gray_skin6, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours_skin, hierarchy = cv2.findContours(eroded_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     # Drawing bounding boxes, counting faces
-    for contour1 in contours_skin1:
-        x, y, w, h = cv2.boundingRect(contour1)
-        cv2.rectangle(img_result, (x, y), (x + w, y + h), (0, 0, 255), 2)
-        faces = faces+1
-    for contour2 in contours_skin2:
-        x, y, w, h = cv2.boundingRect(contour2)
-        cv2.rectangle(img_result, (x, y), (x + w, y + h), (0, 0, 255), 2)
-        faces = faces + 1
-    for contour3 in contours_skin3:
-        x, y, w, h = cv2.boundingRect(contour3)
-        cv2.rectangle(img_result, (x, y), (x + w, y + h), (0, 0, 255), 2)
-        faces = faces + 1
-    for contour4 in contours_skin4:
-        x, y, w, h = cv2.boundingRect(contour4)
-        cv2.rectangle(img_result, (x, y), (x + w, y + h), (0, 0, 255), 2)
-        faces = faces + 1
-    for contour5 in contours_skin5:
-        x, y, w, h = cv2.boundingRect(contour5)
-        cv2.rectangle(img_result, (x, y), (x + w, y + h), (0, 0, 255), 2)
-        faces = faces + 1
-    for contour6 in contours_skin6:
-        x, y, w, h = cv2.boundingRect(contour6)
-        cv2.rectangle(img_result, (x, y), (x + w, y + h), (0, 0, 255), 2)
-        faces = faces + 1
+    for contour in contours_skin:
+        x, y, w, h = cv2.boundingRect(contour)
+        cv2.rectangle(img_result, (x, y), (x + w, y + h), (0, 0, 255), 3)
+        personnel_number = personnel_number + 1
 
-    return img_result, faces
-
-'''
-img = cv2.imread('teszt_kepek/picture_02.jpg')
-res, count = rgb_detect(img)
-cv2.imshow('res',res)
-cv2.waitKey()
-'''
+    return img_result, personnel_number
